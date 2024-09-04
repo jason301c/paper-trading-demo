37d04bcca16661b5cdd8afbd004fdef4e9154b49
JasonA <jasonalex.st@gmail.com>
2024-09-04 19:32:11 +1000

    Use Montserrat font globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
